4dfc46d032dfba10fb978c46a8f25961b0d8fcbb
villian26 <llkingll2627@gmail.com>
2024-12-24 13:16:11 +0530

    Update tagall.py

--- nexichat/idchatbot/tagall.py
+++ nexichat/idchatbot/tagall.py
@@ -1,93 +1,55 @@
 import asyncio
 from pyrogram import Client, filters
 from nexichat.idchatbot.helpers import is_owner
+
 SPAM_CHATS = []
 
-
-@Client.on_message(
-    filters.command(["all", "mention", "tagall", "mentionall"], prefixes=["."])
-)
+@Client.on_message(filters.command(["all", "mention", "tagall", "mentionall"], prefixes=["."]))
 async def tag_all_users(client, message):
     clone_id = (await client.get_me()).id
     user_id = message.from_user.id
     if not await is_owner(clone_id, user_id):
         await message.reply_text("You don't have permission to use this command on this bot.")
         return
+    
     if message.chat.id in SPAM_CHATS:
-        return await message.reply_text(
-            "ᴛᴀɢɢɪɴɢ ᴘʀᴏᴄᴇss ɪs ᴀʟʀᴇᴀᴅʏ ʀᴜɴɴɪɴɢ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sᴛᴏᴘ sᴏ ᴜsᴇ /cancel"
-        )
-
-    replied = message.reply_to_message
-    if len(message.command) < 2 and not replied:
-        await message.reply_text(
-            "** ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ »** `.all Hi Friends`"
-        )
+        await message.reply_text("Tagging process is already running. Use /cancel to stop.")
         return
-    if replied:
-        SPAM_CHATS.append(message.chat.id)
-        usernum = 0
-        usertxt = ""
-        async for m in client.get_chat_members(message.chat.id):
-            if message.chat.id not in SPAM_CHATS:
-                break
-            usernum += 3
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
-            if usernum == 1:
-                await replied.reply_text(usertxt)
-                await asyncio.sleep(5)
-                usernum = 0
-                usertxt = ""
-        try:
-            SPAM_CHATS.remove(message.chat.id)
-        except Exception:
-            pass
-    else:
-        text = message.text.split(None, 1)[1]
-
-        SPAM_CHATS.append(message.chat.id)
-        usernum = 0
-        usertxt = ""
+    
+    text = message.text.split(None, 1)[1] if len(message.command) > 1 else None
+    if not text and not message.reply_to_message:
+        await message.reply_text("Please provide text to tag all, like » `.all Hi Friends`")
+        return
+    
+    SPAM_CHATS.append(message.chat.id)
+    usernum = 0
+    usertxt = ""
+    try:
         async for m in client.get_chat_members(message.chat.id):
             if message.chat.id not in SPAM_CHATS:
                 break
             usernum += 1
             usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
             if usernum == 3:
-                await client.send_message(
-                    message.chat.id,
-                    f"{text}\n{usertxt}\n\n|| ➥ ᴏғғ ᴛᴀɢɢɪɴɢ ʙʏ » /cancel ||",
-                )
+                await client.send_message(message.chat.id, f"{text}\n{usertxt}\n\n|| ➥ Off tagging by » /cancel ||")
                 await asyncio.sleep(5)
                 usernum = 0
                 usertxt = ""
+        if message.reply_to_message:
+            await message.reply_to_message.reply_text(usertxt)
+        else:
+            await client.send_message(message.chat.id, f"{text}\n{usertxt}\n\n|| ➥ Off tagging by » /cancel ||")
+    finally:
         try:
             SPAM_CHATS.remove(message.chat.id)
         except Exception:
             pass
 
-
-@Client.on_message(
-    filters.command(
-        [
-            "stopmention",
-            "stoptagall",
-            "canceltagall",
-            "offall",
-            "cancel",
-            "allstop",
-            "stopall",
-            "cancelmention",
-            "offmention",
-            "mentionoff",
-            "alloff",
-            "cancelall",
-            "allcancel",
-        ],
-        prefixes=[".", "/"],
-    )
-    
-)
+@Client.on_message(filters.command([
+    "stopmention", "stoptagall", "canceltagall", "offall", "cancel",
+    "allstop", "stopall", "cancelmention", "offmention", "mentionoff",
+    "alloff", "cancelall", "allcancel"
+], prefixes=[".", "/"]))
 async def cancelcmd(client, message):
     chat_id = message.chat.id
     
@@ -96,8 +58,6 @@ async def cancelcmd(client, message):
             SPAM_CHATS.remove(chat_id)
         except Exception:
             pass
-        return await message.reply_text("**ᴛᴀɢɢɪɴɢ ᴘʀᴏᴄᴇss sᴜᴄᴄᴇssғᴜʟʟʏ sᴛᴏᴘᴘᴇᴅ!**")
-
+        await message.reply_text("Tagging process successfully stopped!")
     else:
-        await message.reply_text("**ɴᴏ ᴘʀᴏᴄᴇss ᴏɴɢᴏɪɴɢ!**")
-        return
+        await message.reply_text("No process ongoing!")
