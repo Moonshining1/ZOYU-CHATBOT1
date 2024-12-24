4b72f3e450ae178a39ff500b93b8790e945c813d
villian26 <llkingll2627@gmail.com>
2024-12-24 13:03:14 +0530

    Update sudoers.py

--- nexichat/database/sudoers.py
+++ nexichat/database/sudoers.py
@@ -1,28 +1,38 @@
 from nexichat import mongodb
-from typing import Dict, List, Union
+from typing import List
 
 sudoersdb = mongodb.sudoers
 
-async def get_sudoers() -> list:
-    sudoers = sudoersdb.find_one({"sudo": "sudo"})
-    if not sudoers:
+async def get_sudoers() -> List[int]:
+    try:
+        sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+        return sudoers["sudoers"] if sudoers else []
+    except Exception as e:
+        print(f"Error in get_sudoers: {e}")
         return []
-    return sudoers["sudoers"]
-
 
 async def add_sudo(user_id: int) -> bool:
-    sudoers = await get_sudoers()
-    sudoers.append(user_id)
-    sudoersdb.update_one(
-        {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
-    )
-    return True
-
+    try:
+        sudoers = await get_sudoers()
+        if user_id not in sudoers:
+            sudoers.append(user_id)
+            await sudoersdb.update_one(
+                {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
+            )
+        return True
+    except Exception as e:
+        print(f"Error in add_sudo: {e}")
+        return False
 
 async def remove_sudo(user_id: int) -> bool:
-    sudoers = await get_sudoers()
-    sudoers.remove(user_id)
-    sudoersdb.update_one(
-        {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
-    )
-    return True
+    try:
+        sudoers = await get_sudoers()
+        if user_id in sudoers:
+            sudoers.remove(user_id)
+            await sudoersdb.update_one(
+                {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
+            )
+        return True
+    except Exception as e:
+        print(f"Error in remove_sudo: {e}")
+        return False
