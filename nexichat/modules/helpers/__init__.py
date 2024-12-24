e7fa86288be9918149bc189668d3f1de3dc1fe58
villian26 <llkingll2627@gmail.com>
2024-12-24 12:57:21 +0530

    Update __init__.py

--- nexichat/modules/helpers/__init__.py
+++ nexichat/modules/helpers/__init__.py
@@ -1,22 +1,23 @@
 from typing import Callable
-
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import Message
-
 from nexichat import OWNER, nexichat
-
 
 def is_admins(func: Callable) -> Callable:
     async def non_admin(c: nexichat, m: Message):
-        if m.from_user.id == OWNER:
-            return await func(c, m)
+        try:
+            if m.from_user.id == OWNER:
+                return await func(c, m)
 
-        admin = await c.get_chat_member(m.chat.id, m.from_user.id)
-        if admin.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-            return await func(c, m)
+            admin = await c.get_chat_member(m.chat.id, m.from_user.id)
+            if admin.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+                return await func(c, m)
+        except Exception as e:
+            # Log the exception or handle it as necessary
+            print(f"Error in is_admins decorator: {e}")
+            return
 
     return non_admin
-
 
 from .inline import *
 from .read import *
